b38c2ba8fc9a5d140b19c5973e95c289f378b896
wwdfdo <wwdfdo@gmail.com>
2022-11-15 22:32:53 +0530

    added new styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="w-full flex justify-center">
-      <div className=" fixed flex justify-between min-h-[100px] items-center w-[95%] mx-auto z-10  ">
+    <div className="w-full flex justify-center ">
+      <div className=" fixed flex justify-between min-h-[100px] items-center w-full px-12 z-10 ">
         <div className="p-2 bg-gradient-to-r  rounded-3xl font-extrabold text-xl text-white">
           <img className="w-[180px]" src={logo} alt="" />
         </div>
@@ -14,7 +14,7 @@ function Header() {
           {menus.map((menu) => (
             <Link
               to={menu.url}
-              className="focus:underline focus:bg-opacity-80 hover:bg-opacity-80 bg-opacity-40  tracking-[.1825rem] font-medium text-[1.1rem] px-3 h-7 text-gray-200 hover:text-[#ff0000] text-center rounded-md flex items-center cursor-pointer"
+              className="focus:text-[#ff0000] focus:bg-opacity-80 hover:bg-opacity-80 bg-opacity-40  tracking-[.07rem] font-medium text-[1.1rem] px-3 h-7 text-gray-200 hover:text-[#ff0000] text-center rounded-md flex items-center cursor-pointer"
             >
               <li> {menu.name}</li>
             </Link>
