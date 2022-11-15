ac33b4b6aa5ae3f0c6f574dfb39f9b3c5baf049f
wwdfdo <wwdfdo@gmail.com>
2022-11-15 21:16:20 +0530

    added new fong

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       backgroundImage: {
         viewportBg: "url('/src/images/stadium7.jpg')",
         stadium2: "url('/src/images/stadium2.webp')",
