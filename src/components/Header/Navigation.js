d68209b5a460ec1a08a085df3381f1e437635000
wwdfdo <wwdfdo@gmail.com>
2022-12-02 10:10:32 +0530

    added new login page

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../store/auth-context";
 
-function Navigation({ menus }) {
+function Navigation() {
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
